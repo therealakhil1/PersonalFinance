bd462f3f8d788ec4040f80180860a83a84346d23
therealakhil1 <akhilspalli@gmail.com>
2024-11-15 20:00:52 +0530

    Initial Commit

---
+++ PersonalFinance/PersonalFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceApp.swift
+//  PersonalFinance
+//
+//  Created by Akhil on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PersonalFinanceTests/PersonalFinanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceTests.swift
+//  PersonalFinanceTests
+//
+//  Created by Akhil on 15/11/24.
+//
+
+import Testing
+@testable import PersonalFinance
+
+struct PersonalFinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
